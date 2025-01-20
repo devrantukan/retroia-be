3f0a9ef4805a7842a80ec64c99e1f9159e83af82
devrantukan <devrantukan@gmail.com>
2025-01-20 16:37:28 +0300

    fix: damn button fix :)

--- src/app/components/UserProfilePanel.tsx
+++ src/app/components/UserProfilePanel.tsx
@@ -89,14 +89,14 @@ const UserProfilePanel = ({ user, role = null }: Props) => {
           key="logout"
           color="danger"
           className="text-danger p-0 h-full"
-          onClick={(e) => {
-            e.preventDefault();
-            window.location.href = "/api/auth/logout";
-          }}
         >
-          <span className="w-full h-full px-2 py-2 inline-flex items-center">
-            Çıkış Yap
-          </span>
+          <div className="w-full h-full">
+            <LogoutLink postLogoutRedirectURL="/">
+              <div className="w-full h-full px-2 py-2 inline-flex items-center text-danger cursor-pointer">
+                Çıkış Yap
+              </div>
+            </LogoutLink>
+          </div>
         </DropdownItem>
       </DropdownMenu>
     </Dropdown>
